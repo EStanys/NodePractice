33007f406e6c4a0289d91f9e062b40c40036ce5a
EStanys <edvinasstanys@gmail.com>
2021-05-18 15:37:41 +0300

    PascalCAse Function Created

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
-const stringToCamelCase = require("./functions");
+const { stringToCamelCase, stringToPaskalCase } = require("./functions");
 
-console.log("res", stringToCamelCase("viskas puikiai veikia"));
+console.log("stringToCamelCase", stringToCamelCase("viskas puikiai veikia"));
+console.log("stringToPaskalCase", stringToPaskalCase("viskas puikiai veikia"));
--- functions.js
+++ functions.js
@@ -1,13 +1,19 @@
 const stringToCamelCase = (string) => {
-  console.log(
-    "res:",
-    string
-      .split(" ")
-      .map((word) => {
-        return word[0].toUpperCase() + word.slice(1);
-      })
-      .join("")
-  );
+  const cammelCase = string
+    .split(" ")
+    .map((word) => {
+      return word[0].toUpperCase() + word.slice(1);
+    })
+    .join("");
+  return cammelCase[0].toLowerCase() + cammelCase.slice(1);
 };
 
-module.exports = stringToCamelCase;
+const stringToPaskalCase = (string) => {
+  return string
+    .split(" ")
+    .map((word) => {
+      return word[0].toUpperCase() + word.slice(1);
+    })
+    .join("");
+};
+module.exports = { stringToCamelCase, stringToPaskalCase };
