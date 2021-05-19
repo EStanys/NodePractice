dffb63c0e6c10cabe2aeab79640d02fce5d285ad
EStanys <edvinasstanys@gmail.com>
2021-05-19 10:20:41 +0300

    prompt installed and function created

---
+++ js/prompt.js
@@ -0,0 +1,23 @@
+const prompt = require("prompt");
+const {
+  stringToCamelCase,
+  stringToPaskalCase,
+  stringToSnakeCase,
+  stringToKebabCase,
+  getRandomNuber,
+} = require("../functions");
+//
+// Start the prompt
+//
+prompt.start();
+
+//
+// Get two properties from the user: username and email
+//
+prompt.get(["YourSentense"], function (err, result) {
+  //
+  // Log the results.
+  //
+  console.log("Command-line input received:");
+  console.log("Sentence to kebab case:", stringToKebabCase(result.YourSentense));
+});
